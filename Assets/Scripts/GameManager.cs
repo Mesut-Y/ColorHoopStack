854cedef92dfd92eaa3586a568849f6503e70032
mesutyanmaz <71411632+mesutyanmaz@users.noreply.github.com>
2025-02-07 00:39:56 +0300

    prototoip tasarım tamamlandı. Stand ve çemver için sınıf oluşturuldu. GameManager başlangıç

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using Unity.Multiplayer.Center.Common;
+using UnityEngine;
+using UnityEngineInternal;
+
+public class GameManager : MonoBehaviour
+{
+    GameObject selectedObject;
+    GameObject selectedPlatform;
+    Hoop _cember;
+    public bool isMove;
+
+    //they are initialized for after
+    public int targetStandCount;
+    int completedStandCount;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, 100))
+            {
+                if (hit.collider != null && hit.collider.CompareTag("Stand"))
+                {
+                    //Debug.Log("Standa tıklandı.");
+
+                    if (selectedObject != null && selectedPlatform != hit.collider.gameObject)
+                    { // çember hedef bir yere gönderilecek
+
+                    }
+                    else
+                    {
+                        Stand _stand = hit.collider.GetComponent<Stand>();
+                        selectedObject = _stand.GetLastHook();
+                        _cember = selectedObject.GetComponent<Hoop>();
+                        isMove = true;
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Stand.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class Stand : MonoBehaviour
+{
+    public GameObject targetPoint;  //halka çıkış noktası
+    public GameObject[] sockets;
+    public int emptySocket; //soketboşluğu
+    public List<GameObject> _hooks = new(); //Standaki soketteki halkalar
+
+    [SerializeField] private GameManager _GameManager;
+
+    public GameObject GetLastHook()
+    {
+        
+        return _hooks[^1]; //_hooks[_Cemberler.Count-1];
+
+    }
+}
